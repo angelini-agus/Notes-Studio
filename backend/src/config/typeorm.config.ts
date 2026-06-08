414ea96b50e910d4e8db3ec4ebf5bb52b586a583
Aguss <angeliniagustin935@gmail.com>
2026-06-08 15:22:12 -0300

    fix: allow self-signed certificate for Aiven DB connection

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -3,16 +3,29 @@ import { DataSourceOptions } from 'typeorm';
 import * as fs from 'fs';
 
 export function getTypeOrmConfig(): DataSourceOptions {
-  const sslConfig = {
-    rejectUnauthorized: false, // <-- Cambiar esto de true a false
-    ca: fs.readFileSync(join(process.cwd(), 'ca.pem')).toString(),
-  };
+  const isProduction = !!process.env.DATABASE_URL;
+
+  const sslConfig = isProduction
+    ? {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(join(process.cwd(), 'ca.pem')).toString(),
+      }
+    : false;
 
   return {
     type: 'postgres',
     url: process.env.DATABASE_URL,
+
+    // Configuración para LOCAL
+    host: String(process.env.POSTGRES_HOST ?? 'localhost'),
+    port: Number(process.env.POSTGRES_PORT ?? 5432),
+    username: String(process.env.POSTGRES_USERNAME ?? 'postgres'),
+    password: String(process.env.POSTGRES_PASSWORD ?? ''),
+    database: String(process.env.POSTGRES_DATABASE ?? 'notes_app'),
+
     entities: [join(__dirname, '..', '**', '*.entity.{ts,js}')],
     synchronize: true,
+
     ssl: sslConfig,
     extra: { ssl: sslConfig },
   };
